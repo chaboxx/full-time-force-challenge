85a3c536b091a70b802469b8842e2837d68c964f
unknown <chaboxx159@gmail.com>
2022-05-20 19:37:01 -0500

    p v1.0

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,32 +1,42 @@
-import { url } from 'inspector';
-import { nanoid } from 'nanoid';
 import { FC, useEffect, useState } from 'react';
+
 import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai';
-import { TableInfo } from '../components/homeScreen/TableInfo';
-import { Footer } from '../components/ui/Footer';
+import { nanoid } from 'nanoid';
+
 import { Navbar } from '../components/ui/Navbar';
+import { Footer } from '../components/ui/Footer';
+
+import { TableInfo } from '../components/homeScreen/TableInfo';
+
 import { useApi } from '../hooks/useApi';
-import { GetCommitData, GitHubCommit } from '../interfaces/github-response';
-
-// import { Link } from 'react-router-dom';
-
 
 import styles from "../styles/pages/HomeScreen.module.css";
+
+
+import { GitHubCommit } from '../interfaces/github-response';
+
+
 
 export const HomeScreen : FC = () => {
   
   const { getCommitData } = useApi();
+  
   const [checking, setChecking] = useState(true);
   
   const [commitsInformationData, setCommitsInformationData] = useState<GitHubCommit[]>([]);
   const [auxCommitsInformationData, setAuxCommitsInformationData] = useState<GitHubCommit[]>([]);
+  
   const [dataLength, setdataLength] = useState(0);
   const [auxDataLength, setAuxDataLength] = useState(0);
 
   const [actualPage, setActualPage] = useState(1);
-  
+
+
   const [urlGitHubRepo, setUrlGitHubRepo] = useState("");
-  
+  const [auxUrlGitHubRepo, setAuxUrlGitHubRepo] = useState("");
+
+
+
   const handleChangeRepo = async ( e : React.FormEvent<HTMLFormElement>) =>{
     e.preventDefault();
     if ( !urlGitHubRepo.trim() ){
@@ -39,11 +49,31 @@ export const HomeScreen : FC = () => {
       alert("Error in the server . Try Again");
       return;
     }
+    setAuxUrlGitHubRepo(urlGitHubRepo);
     setActualPage(1);
     setAuxCommitsInformationData(resp!.data);
     setAuxDataLength(resp.dataLength);
   }
 
+  const handleGetMoreRepoData = async() =>{
+    if ( auxCommitsInformationData!.length===0 ){
+      const resp = await getCommitData(actualPage);
+      if( !resp ){
+        alert("Error in the server . Try Again");
+        return;
+      }
+      setCommitsInformationData(resp.data);
+      setdataLength(resp.dataLength)
+    }else{
+      const resp = await getCommitData(actualPage,auxUrlGitHubRepo);
+      if( !resp ){
+        alert("Error in the server . Try Again");
+        return;
+      }
+      setAuxCommitsInformationData(resp.data);
+      setAuxDataLength(resp.dataLength)
+    }
+  }
   const handleCommitDataDefault = ( e:React.MouseEvent<HTMLButtonElement, MouseEvent> ) => {
     setActualPage(1);
     setAuxCommitsInformationData([]);
@@ -91,29 +121,40 @@ export const HomeScreen : FC = () => {
           <div className={styles.pagination_info}>
             <div onClick={(e)=>{
               if ( auxCommitsInformationData!.length===0 ){
-                setdataLength(dataLength=>dataLength > 1 ? dataLength-1 : dataLength );
+                setActualPage(actualPage=>actualPage > 1 ? actualPage - 1 : actualPage );
               }else{
-                setAuxDataLength(auxDataLength=>auxDataLength > 1 ? auxDataLength-1 : auxDataLength);
+                setActualPage(actualPage=>actualPage > 1 ? actualPage - 1 : actualPage);
               }
+              handleGetMoreRepoData();
             }} className={[styles.left_icon_container , "pointer"].join(" ")}>
               <AiFillCaretLeft/>
             </div>
             <div className={styles.page_container}>
 
             {
-              new Array(Math.floor(dataLength/6)).fill("").map((arr,index)=>(
-                <p key={nanoid()} className={styles.label_actual_page}>
-                {index+1}
-              </p> 
+              new Array(Math.ceil(auxCommitsInformationData!.length===0 ? dataLength/6 : auxDataLength/6)).fill("").map((arr,index)=>(
+                <p onClick={(e)=>{
+                  setActualPage(index+1);
+                  if(actualPage !==index+1){
+                    handleGetMoreRepoData();
+                  }
+                }}
+                  key={nanoid()}
+                  className={[ actualPage ===index+1 ?  styles.label_actual_page : styles.label_page , "hover"].join(" ")}
+                >
+                  {index+1}
+                </p> 
               ))
             }
             </div>
             <div onClick={(e)=>{
               if ( auxCommitsInformationData!.length===0 ){
-                setdataLength(dataLength=>dataLength < Math.floor(dataLength/6) ? dataLength + 1 : dataLength );
+                setActualPage(actualPage=>actualPage < Math.floor(dataLength/6) ? actualPage + 1 : actualPage );
               }else{
-                setAuxDataLength(auxDataLength=>auxDataLength < Math.floor(auxDataLength/6) ? auxDataLength + 1 : auxDataLength);
+                setActualPage(actualPage=>actualPage < Math.floor(auxDataLength/6) ? actualPage + 1 : actualPage);
               }
+              
+            
             }} className={[styles.rigth_icon_container, "pointer"].join(" ")}>
               <AiFillCaretRight/>
 
